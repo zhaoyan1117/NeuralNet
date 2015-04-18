0693b30acb72a1e64db8b8bfc04823cf2cf6232d
Yan Zhao <zhaoyan1117@gmail.com>
2015-04-18 16:03:17 -0700

    wip

--- nnet/neural_net.py
+++ nnet/neural_net.py
@@ -4,29 +4,45 @@ from .neural_net_exception import NeuralNetException
 
 class NeuralNet(object):
 
-    def __init__(self, batch_size, lr_func, loss_func, layers):
+    def __init__(self, batch_size, lr_func, loss_func, layers, loss_period):
         self.batch_size = batch_size
         self.lr_func = lr_func
         self.loss_func = loss_func
         self.layers = layers
         self.data, self.labels = None, None
+        self.loss_period = loss_period
 
     def train(self, data, labels):
         pass
 
-    def forward_p(self):
-        pass
+    def forward_p(self, data):
+        cur_z = data
+        for l in self.layers:
+            cur_z = l.forward_p(cur_z)
+        return cur_z
 
-    def backward_p(self):
-        pass
+    def backward_p(self, delta):
+        cur_delta = delta
+        for l in reversed(self.layers):
+            cur_delta = l.backward_p(cur_delta)
+
+    def update(self, lr):
+        for l in self.layers:
+            l.update(lr)
 
     def numerical_check(self):
-        pass
+        for l in self.layers:
+            passed = l.numerical_check(self)
+            if not passed:
+                print "WARNING: layer {0} " \
+                      "failed numerical check.".format(l.level)
 
-    def compute_loss(self, data, labels):
-        pass
+    def compute_loss(self, labels, predictions):
+        return self.loss_func.apply(labels, predictions)
 
     def compute_all_loss(self):
         if self.data is None:
             raise NeuralNetException('Cannot compute loss without data.')
-        return self.compute_loss(self.data, self.labels)
+
+        return self.compute_loss(self.labels,
+                                 self.forward_p(self.data))
