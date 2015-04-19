fix numerical checking

nnet/_neural_net.py
@@ -15,9 +15,9 @@ class NeuralNet(object):
         self.stopping_c = stopping_c
         self.layers = layers
 
-        self.data, self.labels = None, None
+        self.data, self.vec_labels = None, None
 
-        self.status_period = kwargs.get('status_period', 1000)
+        self.status_period = kwargs.get('status_period', 10000)
         self.check_period = kwargs.get('check_period')
 
     def train(self, data, labels):
@@ -25,7 +25,7 @@ class NeuralNet(object):
         losses = np.empty((0, 3))
 
         # Shuffle input data and labels.
-        self.data, self.labels = \
+        self.data, self.vec_labels = \
             shuffle_data_labels(data, vectorize_labels(labels, len(self.klasses)))
 
         data_i = 0
@@ -34,7 +34,7 @@ class NeuralNet(object):
         while not self.stopping_c.stop(self):
             # Slice data and labels for this epoch.
             cur_data = self.data[data_i:data_i+self.batch_size]
-            cur_label = self.labels[data_i:data_i+self.batch_size]
+            cur_label = self.vec_labels[data_i:data_i+self.batch_size]
 
             # Forward propagation.
             y_hat = self._forward_p(cur_data)
@@ -67,7 +67,7 @@ class NeuralNet(object):
                                      self.compute_all_loss(),
                                      self.score(data, labels)]],
                                    axis=0)
-                self.data, self.labels = \
+                self.data, self.vec_labels = \
                     shuffle_data_labels(data, vectorize_labels(labels, len(self.klasses)))
                 data_i = 0
                 self.cur_epoch += 1
@@ -99,16 +99,19 @@ class NeuralNet(object):
     def _numerical_check(self):
         for l in self.layers:
             passed = l.numerical_check(self)
-            if not passed:
+            if passed:
+                print "Layer {0} " \
+                      "passed numerical check.".format(l.level)
+            else:
                 print "WARNING: layer {0} " \
                       "failed numerical check.".format(l.level)
 
-    def _compute_loss(self, labels, predictions):
-        return self.loss_func.apply(labels, predictions)
+    def _compute_loss(self, vec_labels, predictions):
+        return self.loss_func.apply(vec_labels, predictions)
 
     def compute_all_loss(self):
         if self.data is None:
             raise NeuralNetException('Cannot compute loss without data.')
 
-        return self._compute_loss(self.labels,
+        return self._compute_loss(self.vec_labels,
                                   self._forward_p(self.data))

nnet/layer/_fully_connected_layer.py
@@ -3,6 +3,7 @@ from __future__ import absolute_import, division
 import numpy as np
 
 from ._base import LayerBase
+from ..util import iterate_with_progress
 
 class FullyConnectedLayer(LayerBase):
 
@@ -15,7 +16,7 @@ class FullyConnectedLayer(LayerBase):
         self.a = None
         self.z = None
         self.delta = None
-        self.weights_derivative = None
+        self.weights_grad = None
 
     def set_next_layer_size(self, next_size):
         self.next_size = next_size
@@ -39,7 +40,7 @@ class FullyConnectedLayer(LayerBase):
         return next_z
 
     def backward_p(self, next_delta):
-        self.weights_derivative = np.dot(self.a.T, next_delta)
+        self.weights_grad = np.dot(self.a.T, next_delta)
 
         # No need to compute delta if layer is the first layer.
         if self.level != 1:
@@ -51,7 +52,7 @@ class FullyConnectedLayer(LayerBase):
         return self.delta
 
     def update(self, lr):
-        self.weights = self.weights - lr * self.weights_derivative
+        self.weights = self.weights - lr * self.weights_grad
 
     def numerical_check(self, net):
         epsilon = 1e-5
@@ -59,22 +60,27 @@ class FullyConnectedLayer(LayerBase):
         num_grad = np.zeros(self.weights.shape)
         perturb = np.zeros(self.weights.shape)
 
-        for i in xrange(len(current_weights)):
-            for j in xrange(len(current_weights[i])):
-                perturb[i][j] = epsilon
+        total_size = current_weights.shape[0] * current_weights.shape[1]
 
-                self.weights = current_weights - perturb
-                loss1 = net.compute_all_loss()
+        for k in iterate_with_progress(xrange(total_size)):
+            i = k % current_weights.shape[0]
+            j = k // current_weights.shape[0]
 
-                self.weights = current_weights + perturb
-                loss2 = net.compute_all_loss()
+            perturb[i][j] = epsilon
 
-                num_grad[i][j] = (loss2 - loss1) / (2*epsilon)
+            self.weights = current_weights - perturb
+            loss1 = net.compute_all_loss()
 
-                perturb[i][j] = 0.0
+            self.weights = current_weights + perturb
+            loss2 = net.compute_all_loss()
+
+            num_grad[i][j] = (loss2 - loss1) / (2*epsilon)
+            # print self.weights_grad[i][j] - num_grad[i][j]
+
+            perturb[i][j] = 0.0
 
         self.weights = current_weights
 
-        diff = (self.weights_derivative - num_grad).ravel()
-
-        return np.linalg.norm(diff) < 1e-8
+        diff = np.linalg.norm((self.weights_grad - num_grad).ravel())
+        sum = np.linalg.norm((self.weights_grad + num_grad).ravel())
+        return diff/sum < 1e-8