14e2652500161343778bd0a468a3ed3a74ca9a8c
Yan Zhao <zhaoyan1117@gmail.com>
2015-04-18 17:55:35 -0700

    added setup file

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+__version__ = '0.1'
+
+setup(
+    name='nnet',
+    version=__version__,
+    url='https://github.com/zhaoyan1117/NeuralNet',
+    packages=find_packages(),
+    include_package_data=True,
+)
