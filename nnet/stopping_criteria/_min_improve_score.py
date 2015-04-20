8a616a0bea41837c17cc009801257eb69124f4e4
Yan Zhao <zhaoyan1117@gmail.com>
2015-04-20 14:46:42 -0700

    update min improve sc

--- nnet/stopping_criteria/_min_improve_score.py
+++ nnet/stopping_criteria/_min_improve_score.py
@@ -10,40 +10,42 @@ class MinImproveScore(StoppingCriteriaBase):
     def __init__(self, k, threshold):
         self.k = k
         self.threshold = threshold
-        self.last_epoch = None
+        self.last_update = None
         self.counter = 0
 
     def stop(self, net):
-        latest_epoch = net.cur_epoch
+        latest_update = len(net.losses)
 
-        if self.last_epoch == latest_epoch:
-            # Already checked for this epoch.
+        if self.last_update == latest_update:
+            # Already checked for this update.
             return False
-        elif latest_epoch < 2 * self.k:
+        elif latest_update < 2 * self.k:
             # Not enough data.
             return False
         else:
-            self.last_epoch = latest_epoch
+            self.last_update = latest_update
 
             cur_score = np.mean(
-                net.losses[latest_epoch-self.k:latest_epoch, 1]
+                net.losses[latest_update-self.k:latest_update, 1]
             )
 
             last_score = np.mean(
-                net.losses[latest_epoch-2*self.k:latest_epoch-self.k, 1]
+                net.losses[latest_update-2*self.k:latest_update-self.k, 1]
             )
 
             if cur_score - last_score > self.threshold:
                 return False
             else:
-                print('Stop at EPOCH {epoch}, '
+                print('Stop at epoch {epoch}, '
+                      'iteration {iteration}\n'
                       'average score of last {k} epochs '
                       'improved smaller than {threshold}\n'
                       'Latest score : {latest_score} | '
                       'Latest loss  : {latest_loss}'
-                      .format(epoch = latest_epoch,
-                              k = self.k,
-                              threshold = self.threshold,
-                              latest_score = net.losses[-1,1],
-                              latest_loss = net.losses[-1,2]))
+                      .format(epoch=net.cur_epoch,
+                              iteration=net.cur_iteration,
+                              k=self.k,
+                              threshold=self.threshold,
+                              latest_score=net.losses[-1,2],
+                              latest_loss=net.losses[-1,3]))
                 return True
