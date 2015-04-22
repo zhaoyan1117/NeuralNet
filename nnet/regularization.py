1389808b6044d5900fd182697a7123b147e238f3
Yan Zhao <zhaoyan1117@gmail.com>
2015-04-21 18:50:59 -0700

    reg

--- nnet/regularization.py
+++ nnet/regularization.py
@@ -3,12 +3,17 @@ from __future__ import absolute_import, division
 import numpy as np
 
 def normalize(data):
-    normalized = np.zeros(data.shape)
-    means = np.mean(data, axis=1)
-    stds = np.std(data, axis=1)
+    def normalize_col(col):
+        mean_col = np.mean(col)
+        std_col = np.std(col)
+        if std_col != 0.0:
+            return (col - mean_col) / std_col
+        else:
+            return (col - mean_col)
+    return np.apply_along_axis(normalize_col, 0, data)
 
-    for i in xrange(len(data)):
-        normalized[i] = (data[i] - means[i]) \
-                        / stds[i]
+def log_plus(data):
+    return np.log(data + 0.1)
 
-    return normalized
+def binarize(data):
+    return (data > 0).astype(data.dtype)
