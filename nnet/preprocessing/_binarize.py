a0343dd469e981bf9b4f18db0209ca9bfaf58c4f
Yan Zhao <zhaoyan1117@gmail.com>
2015-05-11 23:06:25 -0700

    added threshold

--- nnet/preprocessing/_binarize.py
+++ nnet/preprocessing/_binarize.py
@@ -5,8 +5,11 @@ from ._base import PreprocessingBase
 
 class Binarize(PreprocessingBase):
 
+    def __init__(self, threshold=0.0):
+        self.threshold = threshold
+
     def fit(self, X):
-        return (X > 0).astype(X.dtype)
+        return (X > self.threshold).astype(X.dtype)
 
     def transform(self, X):
-        return (X > 0).astype(X.dtype)
+        return (X > self.threshold).astype(X.dtype)
